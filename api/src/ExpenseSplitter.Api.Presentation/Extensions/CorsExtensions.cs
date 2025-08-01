3e282261f8f24ca25b8fe9c58f3bd0aab9e582b6
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2025-08-01 10:38:45 +0200

    fix: correct paths for ci/cd

--- api/src/ExpenseSplitter.Api.Presentation/Extensions/CorsExtensions.cs
+++ api/src/ExpenseSplitter.Api.Presentation/Extensions/CorsExtensions.cs
@@ -4,16 +4,10 @@ public static class CorsExtensions
 {
     public static void AddCorsPolicy(this IServiceCollection services)
     {
-        services.AddCors(options =>
-        {
-            options.AddPolicy("AllowLocalhost8080", policy =>
-            {
-                policy
-                    .WithOrigins("http://localhost:8080", "https://localhost:8080")
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials();
-            });
-        });
+        services.AddCors(options => options.AddPolicy("AllowLocalhost8080", policy => policy
+            .WithOrigins("http://localhost:8080", "https://localhost:8080")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()));
     }
 } 
