76c77703324734f8a186eedee6c20572daf9ecf4
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2025-08-01 10:38:45 +0200

    fix: correct paths for ci/cd

--- api/test/ExpenseSplitter.Api.Application.UnitTests/Settlements/LeaveSettlementCommandHandlerTests.cs
+++ api/test/ExpenseSplitter.Api.Application.UnitTests/Settlements/LeaveSettlementCommandHandlerTests.cs
@@ -1,7 +1,9 @@
+using ExpenseSplitter.Api.Application.Abstractions.Authentication;
 using ExpenseSplitter.Api.Application.Settlements.LeaveSettlement;
 using ExpenseSplitter.Api.Domain.Abstractions;
 using ExpenseSplitter.Api.Domain.Settlements;
 using ExpenseSplitter.Api.Domain.SettlementUsers;
+using ExpenseSplitter.Api.Domain.Users;
 
 namespace ExpenseSplitter.Api.Application.UnitTests.Settlements;
 
@@ -9,6 +11,8 @@ public class LeaveSettlementCommandHandlerTests
 {
     private readonly Fixture fixture = CustomFixture.Create();
     private readonly ISettlementUserRepository settlementUserRepository = Substitute.For<ISettlementUserRepository>();
+    private readonly ISettlementRepository settlementRepository = Substitute.For<ISettlementRepository>();
+    private readonly IUserContext userContext = Substitute.For<IUserContext>();
     private readonly IUnitOfWork unitOfWorkMock = Substitute.For<IUnitOfWork>();
     private readonly LeaveSettlementCommandHandler handler;
 
@@ -20,6 +24,8 @@ public class LeaveSettlementCommandHandlerTests
 
         handler = new LeaveSettlementCommandHandler(
             settlementUserRepository,
+            settlementRepository,
+            userContext,
             unitOfWorkMock
         );
     }
@@ -41,6 +47,11 @@ public class LeaveSettlementCommandHandlerTests
     [Fact]
     public async Task Handle_ShouldFail_WhenSettlementWithInviteCodeDoesNotExist()
     {
+        var settlement = fixture.Create<Settlement>();
+        settlementRepository
+            .GetById(Arg.Any<SettlementId>(), Arg.Any<CancellationToken>())
+            .Returns(settlement);
+        
         settlementUserRepository
             .GetBySettlementId(Arg.Any<SettlementId>(), Arg.Any<CancellationToken>())
             .Returns((SettlementUser) default);
@@ -56,6 +67,23 @@ public class LeaveSettlementCommandHandlerTests
     [Fact]
     public async Task Handle_ShouldSuccess()
     {
+        var creatorUserId = new UserId(fixture.Create<Guid>());
+        var currentUserId = new UserId(fixture.Create<Guid>());
+        
+        var settlementResult = Settlement.Create(
+            fixture.Create<string>(),
+            fixture.Create<string>(),
+            creatorUserId,
+            fixture.Create<DateTime>()
+        );
+        var settlement = settlementResult.Value;
+            
+        settlementRepository
+            .GetById(Arg.Any<SettlementId>(), Arg.Any<CancellationToken>())
+            .Returns(settlement);
+            
+        userContext.UserId.Returns(currentUserId);
+
         var command = fixture.Create<LeaveSettlementCommand>();
 
         var response = await handler.Handle(command, default);
