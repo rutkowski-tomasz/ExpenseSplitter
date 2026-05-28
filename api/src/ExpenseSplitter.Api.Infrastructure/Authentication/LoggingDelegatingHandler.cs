2cb3ca70e4397a6e6437d30f39d2d8cd66e38077
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2026-05-28 12:49:35 +0200

    fix: expensive log calls

--- api/src/ExpenseSplitter.Api.Infrastructure/Authentication/LoggingDelegatingHandler.cs
+++ api/src/ExpenseSplitter.Api.Infrastructure/Authentication/LoggingDelegatingHandler.cs
@@ -11,24 +11,32 @@ public class LoggingDelegatingHandler(ILogger<LoggingDelegatingHandler> logger) 
     {
         try
         {
-            var requestContent = await request.Content!.ReadAsStringAsync(cancellationToken);
-            logger.LogInformation(
-                "Sending HTTP request to {Method} {Uri} with {Content}",
-                request.Method.ToString(),
-                request.RequestUri!.ToString(),
-                requestContent
-            );
+            if (logger.IsEnabled(LogLevel.Information))
+            {
+                var requestContent = request.Content is null
+                    ? string.Empty
+                    : await request.Content.ReadAsStringAsync(cancellationToken);
+                logger.LogInformation(
+                    "Sending HTTP request to {Method} {Uri} with {Content}",
+                    request.Method,
+                    request.RequestUri,
+                    requestContent
+                );
+            }
 
             var stopWatch = Stopwatch.StartNew();
             var result = await base.SendAsync(request, cancellationToken);
 
-            var resultContent = await result.Content.ReadAsStringAsync(cancellationToken);
-            logger.LogInformation(
-                "Received HTTP response {StatusCode} in {Duration}ms with {Content}",
-                result.StatusCode,
-                stopWatch.ElapsedMilliseconds,
-                resultContent
-            );
+            if (logger.IsEnabled(LogLevel.Information))
+            {
+                var resultContent = await result.Content.ReadAsStringAsync(cancellationToken);
+                logger.LogInformation(
+                    "Received HTTP response {StatusCode} in {Duration}ms with {Content}",
+                    result.StatusCode,
+                    stopWatch.ElapsedMilliseconds,
+                    resultContent
+                );
+            }
 
             return result;
         }
